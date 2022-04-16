b57aa423414939f93ef92da7ddbf3425f32cc41f
randallkk <randallkk@sookmyung.ac.kr>
2022-04-16 20:57:51 +0900

    renewal

---
+++ ticketscene/src/main/java/pixel/ticketscene/TicketsceneApplication.java
@@ -0,0 +1,13 @@
+package pixel.ticketscene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsceneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketsceneApplication.class, args);
+    }
+
+}
---
+++ ticketscene/src/test/java/pixel/ticketscene/TicketsceneApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.ticketscene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsceneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
